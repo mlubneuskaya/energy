de15617a0b55665298b5681a05f5b7bca6e71443
mlubneuskaya <mlubnewskaja@gmail.com>
2024-04-27 22:55:02 +0200

    parametry

--- demand.py
+++ demand.py
@@ -11,8 +11,8 @@ def step_demand(t, dates, annual_demand=50):
         return date.weekday() in range(5)
 
     def is_day(date):
-        return (date.time() <= time(19, 00)) and (date.time() >= time(8, 00))
+        return (date.time() <= time(18, 00)) and (date.time() >= time(8, 00))
 
-    daily = annual_demand / (250 * 24)
-    return [daily if is_workday(date) and is_day(date) else 0 for date in dates]
+    hourly = annual_demand / (250 * 10)
+    return [hourly if is_workday(date) and is_day(date) else 0 for date in dates]
 
